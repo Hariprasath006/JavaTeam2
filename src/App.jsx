192c7090da97935973c9e0ec62725c907a7ae917
Hariprasath006 <harinataraj2005@gmail.com>
2026-02-23 20:00:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-
+import "./index.css";
 /* STUDENT */
 import StudentDashboard from "./pages/user/StudentDashboard";
 import StudentResources from "./pages/user/StudentResources";
